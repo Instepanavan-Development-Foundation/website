ffc6ff7c851e5cbaeebf9269391bdd776beb22d8
Ruben Shekspir <shekspir55@gmail.com>
2025-02-12 16:00:56 +0400

    another test

--- client/app/invalidate/page.tsx
+++ client/app/invalidate/page.tsx
@@ -1,12 +1,26 @@
 import { revalidatePath } from "next/cache";
 
 // TODO: Write in documentation about this page functionality
-export default async function Invalidate() {
+// https://api.instepanavan.am/admin/settings/webhooks/create
+// Invalidate website on change
+
+async function invalidateCache() {
+  'use server';
   try {
     revalidatePath("/", "layout");
-    return <div>Cache invalidated successfully!</div>;
+    return true;
   } catch (error) {
     console.error("Error invalidating cache:", error);
-    return <div>Error invalidating cache</div>;
+    return false;
   }
+}
+
+export default async function Invalidate() {
+  const success = await invalidateCache();
+  
+  return (
+    <div>
+      {success ? "Cache invalidated successfully!" : "Error invalidating cache"}
+    </div>
+  );
 }
