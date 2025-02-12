353b7db4fecae2c881ba0e906e41a4fd087309e8
Ruben Shekspir <shekspir55@gmail.com>
2025-02-12 14:33:24 +0400

    experimental removed

--- client/next.config.js
+++ client/next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
     domains: ['images.unsplash.com', 'i.pravatar.cc'],
   },
   output: 'standalone',
-  // This ensures all pages are treated as dynamic
-  experimental: {
-    serverActions: true,
-  }
 }
 
 module.exports = nextConfig;
