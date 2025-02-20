965a1dd5774fd49042bd9def9130a842badaf4fe
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2025-02-21 00:30:10 +0400

    fix: add key

--- client/components/Supporters.tsx
+++ client/components/Supporters.tsx
@@ -24,7 +24,7 @@ export default function Supporters({
       <h2 className="text-3xl font-bold mb-8">Աջակիցներ</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
         {slicedContributors.map((contributor, index) => (
-          <Tooltip content={contributor.contributor.fullName}>
+          <Tooltip key={contributor.contributor.slug} content={contributor.contributor.fullName}>
             <Link
               href={`/contributor/${contributor.contributor.slug}`}
               key={index}
