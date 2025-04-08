3549a73b8147b15ea54ddd31af4c2a9f9fcc2607
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2025-04-08 10:43:36 +0400

    feat: add payment initial version

---
+++ server/src/api/payment/services/banking.ts
@@ -0,0 +1,89 @@
+import axios from "axios";
+
+const getParams = ({
+  amount,
+  projectDocumentId,
+  paymentMethod,
+  currencyCode = process.env.CURRENCY_AM,
+}) => ({
+  ClientID: process.env.CLIENT_ID,
+  Amount: amount, // 10 for testing
+  OrderID: 3831011, //TODO: createOrderId logic?
+  Currency: currencyCode,
+  BackURL: process.env.BACK_URL,
+  Username: process.env.PAYMENT_USERNAME,
+  Password: process.env.PAYMENT_PASSWORD,
+  Description: `Donate for project ${projectDocumentId}`,
+  CardHolderID: "sample string", // for subscription mode: some unique id
+  Timeout: Number(process.env.PAYMENT_TIMEOUT),
+  Opaque: JSON.stringify({ projectDocumentId, paymentMethod }),
+});
+
+export default () => ({
+  getPaymentUrl: async ({
+    amount,
+    projectDocumentId,
+    currencyCode,
+    paymentMethod,
+    lang,
+  }) => {
+    try {
+      const url = `${process.env.PAYMENT_API_BASE_URL}/InitPayment`;
+      console.log({url, params:getParams({ amount, projectDocumentId, currencyCode, paymentMethod })});
+      
+      const response = await axios.post(
+        url,
+        getParams({ amount, projectDocumentId, currencyCode, paymentMethod }),
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+
+      if (!response.data.PaymentID) {
+        console.log(
+          "something went wrong in /init-payment",
+          JSON.stringify(response.data, null, 2)
+        );
+
+        return {
+          url: null,
+          errorMessage: "something went wrong with getting payment id",
+        };
+      }
+
+      return {
+        url: `${process.env.PAYMENT_API_DETAILS_URL}?id=${response.data.PaymentID}&lang=${lang}`,
+        errorMessage: null,
+      };
+    } catch (e) {
+      console.log(
+        "something went wrong in /init-payment",
+        JSON.stringify(e, null, 2)
+      );
+
+      return { url: null, errorMessage: e.message };
+    }
+  },
+  getPaymentDetails: async (paymentId: string) => {
+    try {
+      const url = `${process.env.PAYMENT_API_BASE_URL}/GetPaymentDetails`;
+      const response = await axios.post(
+        url,
+        {
+          PaymentID: paymentId,
+          Username: process.env.PAYMENT_USERNAME,
+          Password: process.env.PAYMENT_PASSWORD,
+        },
+        { headers: { "Content-Type": "application/json" } }
+      );
+
+      return response.data;
+    } catch (e) {
+      console.log(
+        "something went wrong in getPaymentDetails",
+        JSON.stringify(e, null, 2)
+      );
+      return null;
+    }
+  },
+});
---
+++ server/src/api/payment/services/strapi.ts
@@ -0,0 +1,109 @@
+import {
+  PAYMENT_LOG_API,
+  PAYMENT_METHOD_API,
+  PROJECT_API,
+  PROJECT_PAYMENT_API,
+} from "../constants/apis";
+import { CURRENCIES } from "../constants/currencies";
+
+export default () => ({
+  createProjectPayment: async ({
+    paymentDetails,
+    projectPaymentMethod,
+    paymentLog,
+    projectDocumentId,
+  }) => {
+    try {
+      await strapi.documents(PROJECT_PAYMENT_API).create({
+        data: {
+          amount: paymentDetails.Amount,
+          currency: paymentDetails.Currency,
+          type: "recurring", // TODO add dynamic value after adding subscription
+          name: paymentDetails.Description,
+          payment_method: projectPaymentMethod.documentId,
+          payment_logs: [paymentLog.documentId], // TODO: check if this is correct and doesn't rewrite previous values
+          project: projectDocumentId,
+        },
+      });
+    } catch (e) {
+      console.log(
+        "something went wrong in createProjectPayment",
+        JSON.stringify(e, null, 2)
+      );
+    }
+  },
+  updateProjectData: async ({ projectDocumentId, data }) => {
+    try {
+      await strapi.documents(PROJECT_API).update({
+        documentId: projectDocumentId,
+        data: data,
+      });
+    } catch (e) {
+      console.log(
+        "something went wrong in updateProjectData",
+        JSON.stringify(e, null, 2)
+      );
+    }
+  },
+  getProjectPaymentMethod: async (paymentMethod: string) => {
+    try {
+      return await strapi.documents(PAYMENT_METHOD_API).findFirst({
+        filters: {
+          type: paymentMethod,
+        },
+      });
+    } catch (e) {
+      console.log(
+        "something went wrong in getProjectPaymentMethod",
+        JSON.stringify(e, null, 2)
+      );
+      return null;
+    }
+  },
+  createPaymentLog: async ({
+    paymentDetails,
+    amount,
+  }: {
+    paymentDetails: any;
+    amount?: number;
+  }) => {
+    console.log({
+      amount: amount ?? paymentDetails?.Amount,
+      currency:
+        CURRENCIES[paymentDetails?.Currency ?? process.env.CURRENCY_AM],
+      details: JSON.stringify(paymentDetails),
+    });
+    try {
+      
+      return await strapi.documents(PAYMENT_LOG_API).create({
+        data: {
+          amount: amount ?? paymentDetails.Amount,
+          currency:
+            CURRENCIES[paymentDetails.Currency ?? process.env.CURRENCY_AM],
+          details: JSON.stringify(paymentDetails || {}),
+        },
+      });
+    } catch (e) {
+      console.log(
+        "something went wrong in createPaymentLog",
+        JSON.stringify(e, null, 2)
+      );
+      return null;
+    }
+  },
+  getProject: async (projectDocumentId: string) => {
+    try {
+      return await strapi.documents(PROJECT_API).findOne({
+        documentId: projectDocumentId,
+        status: "published",
+      });
+    } catch (e) {
+      console.log(
+        "something went wrong in getProject",
+        JSON.stringify(e, null, 2)
+      );
+
+      return null;
+    }
+  },
+});
