a834da6461f824987c9111a7965d6c540295c556
Ruben Shekspir <shekspir55@gmail.com>
2026-01-22 12:59:40 +0400

    UI fixed

--- client/next.config.js
+++ client/next.config.js
@@ -1,18 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "localhost",
-      "images.unsplash.com",
-      "i.pravatar.cc",
-      "instepanavan.am",
-      "api.instepanavan.am",
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
+      {
+        protocol: "https",
+        hostname: "instepanavan.am",
+      },
+      {
+        protocol: "https",
+        hostname: "api.instepanavan.am",
+      },
     ],
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
   },
 };
 
