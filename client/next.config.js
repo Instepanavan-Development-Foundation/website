8385ccf0c2d4b839f3045b97d05aa329bfb9406c
Ruben Shekspir <shekspir55@gmail.com>
2026-03-19 22:27:37 +0400

    Switched from rewrites to the proxy file approach.

--- client/next.config.js
+++ client/next.config.js
@@ -1,21 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/ingest/static/:path*",
-        destination: "https://us-assets.i.posthog.com/static/:path*",
-      },
-      {
-        source: "/ingest/array/:path*",
-        destination: "https://us-assets.i.posthog.com/array/:path*",
-      },
-      {
-        source: "/ingest/:path*",
-        destination: "https://us.i.posthog.com/:path*",
-      },
-    ];
-  },
   skipTrailingSlashRedirect: true,
   images: {
     remotePatterns: [
